46ac716be51aabef6b3003f24677b3d4b127adb4
Shashank <81095307+shashank1234-ai@users.noreply.github.com>
2025-03-21 13:45:12 +0530

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,7 +1,7 @@
 UNIQUE_KEY=''
 SUBSCRIBER_ID=''
 PRIVATE_KEY=''
-# obtain access and scret keys from aarambh portal (https://recon.aarambh.cloud)
+# obtain access and scret keys from aarambh portal (https://ondc.aarambh.cloud)
 AARAMBH_ACCESS='' 
 AARAMBH_SECRET=''
 AARAMBH_INTEGRATION_URI='https://integrations.aarambh.cloud/api/v1/integrations'
